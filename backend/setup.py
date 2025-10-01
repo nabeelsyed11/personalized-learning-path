1ad6ab2f0932b6d437bde8e6e0d439070e2c8cd6
inamsyed11 <180098.ec@hkbk.edu.in>
2025-10-02 01:51:02 +0530

    Update deployment configuration and dependencies

--- backend/setup.py
+++ backend/setup.py
@@ -1,10 +1,27 @@
 from setuptools import setup, find_packages
 
+# Read requirements from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="learning_path_backend",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        # Dependencies will be installed from requirements.txt
+    install_requires=requirements,
+    python_requires='>=3.8',
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="Backend for Personalized Learning Path Application",
+    url="https://github.com/nabeelsyed11/personalized-learning-path",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    entry_points={
+        'console_scripts': [
+            'learning-path=app.main:main',
+        ],
+    },
 )
